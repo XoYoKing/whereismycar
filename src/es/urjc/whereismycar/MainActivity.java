40e832c906335d2c09283ed412f021cc184bef0a
Francisco Buitrago Pavon <Buitrago@MacBook-Pro-de-Francisco.local>
2013-05-23 20:36:52 +0200

    inluce design

--- src/es/urjc/whereismycar/MainActivity.java
+++ src/es/urjc/whereismycar/MainActivity.java
@@ -1,12 +1,12 @@
 package es.urjc.whereismycar;
 
+import java.util.List;
 
-
+import com.google.analytics.tracking.android.Log;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import es.urjc.extras.DbHandler;
 import es.urjc.extras.Preferences;
-
 
 import android.location.Location;
 import android.location.LocationListener;
@@ -17,11 +17,15 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothHeadset;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.BluetoothClass.Device;
 import android.content.Context;
 import android.content.Intent;
 
-
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener {
 	private Button btWhereIs;
 	private Button btIamHere;
 	private double CurrentLatitude;
@@ -29,38 +33,42 @@ public class MainActivity extends Activity implements OnClickListener{
 	private DbHandler db;
 	private LocationManager lm;
 	private MyLocationListener ll;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btIamHere = (Button)findViewById(R.id.button1);
-        btWhereIs = (Button)findViewById(R.id.button2);
-        btIamHere.setOnClickListener(this);
-        btWhereIs.setOnClickListener(this);
-        GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        db = new DbHandler(this);
-    }
+	private Activity actv;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		btIamHere = (Button) findViewById(R.id.button1);
+		btWhereIs = (Button) findViewById(R.id.button2);
+		btIamHere.setOnClickListener(this);
+		btWhereIs.setOnClickListener(this);
+		//activateBl();
+		actv = this;
+		GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		db = new DbHandler(this);
+	}
+
 	@Override
 	public void onClick(View v) {
-		
-		switch ( v.getId()) {
+
+		switch (v.getId()) {
 		case R.id.button2:
 			Intent intent = new Intent(this, MyCarActivity.class);
 			startActivity(intent);
 			break;
-			
+
 		case R.id.button1:
 			setCurrentLocation();
 			break;
 
 		default:
-			
+
 			break;
 		}
-		
-		
+
 	}
-	
+
 	private void setCurrentLocation() {
 
 		lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -89,24 +97,21 @@ public class MainActivity extends Activity implements OnClickListener{
 					LocationManager.NETWORK_PROVIDER).getLongitude();
 		}
 		lm.removeUpdates(ll);
-		
-		Preferences.setLatitude(this, (float)CurrentLatitude);
-		Preferences.setLongitude(this, (float)CurrentLongitude);
-		
+
+		Preferences.setLatitude(this, (float) CurrentLatitude);
+		Preferences.setLongitude(this, (float) CurrentLongitude);
+
 		db.addPosition(CurrentLatitude, CurrentLongitude);
-		db.addPosition(40.34486,-3.712867);
-		db.addPosition(40.346086,-3.709069);
-		db.addPosition(40.324954,-3.515139);
-		
-		Toast.makeText(getApplicationContext(),
-				getString(R.string.posSave), Toast.LENGTH_LONG).show();
+		db.addPosition(40.34486, -3.712867);
+		db.addPosition(40.346086, -3.709069);
+		db.addPosition(40.324954, -3.515139);
+
+		Toast.makeText(getApplicationContext(), getString(R.string.posSave),
+				Toast.LENGTH_LONG).show();
 
 	}
 
-	
-	
 	private class MyLocationListener implements LocationListener {
-		
 
 		public void onLocationChanged(Location argLocation) {
 			// TODO Auto-generated method stub
@@ -130,6 +135,66 @@ public class MainActivity extends Activity implements OnClickListener{
 
 	}
 
+	BluetoothHeadset mBluetoothHeadset;
+	private BluetoothProfile.ServiceListener mProfileListener = new BluetoothProfile.ServiceListener() {
+		public void onServiceConnected(int profile, BluetoothProfile proxy) {
+			if (profile == BluetoothProfile.HEADSET) {
+				mBluetoothHeadset = (BluetoothHeadset) proxy;
+				Toast.makeText(getApplicationContext(),
+						getString(R.string.btConected), Toast.LENGTH_LONG)
+						.show();
 
-  
+				if (mBluetoothHeadset != null) {
+					List<BluetoothDevice> devices = mBluetoothHeadset
+							.getConnectedDevices();
+					for (BluetoothDevice dev : devices) {
+					
+						if (mBluetoothHeadset.isAudioConnected(dev)) {
+							Toast.makeText(getApplicationContext(),
+									getString(R.string.btConected),
+									Toast.LENGTH_LONG).show();
+						}
+
+					}
+				}
+			}
+		}
+
+		public void onServiceDisconnected(int profile) {
+			if (profile == BluetoothProfile.HEADSET) {
+				mBluetoothHeadset = null;
+				Toast.makeText(getApplicationContext(),
+						getString(R.string.btDisConected), Toast.LENGTH_LONG)
+						.show();
+
+				setCurrentLocation();
+				Intent intent = new Intent(actv, MyCarActivity.class);
+				startActivity(intent);
+
+			}
+		}
+	};
+
+	// Get the default adapter
+
+	private void activateBl() {
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter
+				.getDefaultAdapter();
+			mBluetoothAdapter.getProfileProxy(this, mProfileListener,
+				BluetoothProfile.HEADSET);
+		if (mBluetoothHeadset != null) {
+			List<BluetoothDevice> devices = mBluetoothHeadset
+					.getConnectedDevices();
+			for (BluetoothDevice dev : devices) {
+				if (mBluetoothHeadset.isAudioConnected(dev)) {
+					Toast.makeText(getApplicationContext(),
+							getString(R.string.btConected), Toast.LENGTH_LONG)
+							.show();
+				}
+
+			}
+		}
+
+	}
+
 }
